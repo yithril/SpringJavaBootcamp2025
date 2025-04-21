136604f59a842bf7bb076798918a196f96e54349
yithril <yithril@gmail.com>
2025-04-21 08:30:47 -0400

    warmup

---
+++ Week2-Strings/4.Thursday/FizzBuzz/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)  {
+        for(int i = 0; i <= 100; i--){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
