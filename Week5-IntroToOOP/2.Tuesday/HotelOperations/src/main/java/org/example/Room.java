22c71a19e42e90302ddd10c0300f69986171d5d3
yithril <yithril@gmail.com>
2025-05-07 09:29:18 -0400

    warmup

--- Week5-IntroToOOP/2.Tuesday/HotelOperations/src/main/java/org/example/Room.java
+++ Week5-IntroToOOP/2.Tuesday/HotelOperations/src/main/java/org/example/Room.java
@@ -1,4 +1,62 @@
 package org.example;
 
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    //isAvailable the derived getter
+    public boolean isAvailable(){
+        if(isOccupied == false && isDirty == false){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public double addTwoNumbers(double a, double b){
+        double sum = a + b;
+        return sum;
+    }
 }
