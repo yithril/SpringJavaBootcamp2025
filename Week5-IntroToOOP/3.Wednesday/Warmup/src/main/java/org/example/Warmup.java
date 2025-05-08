9be4b2ab1f2db88446124e2db6e359a4a5e94c77
yithril <yithril@gmail.com>
2025-05-07 23:58:59 -0400

    warmup

--- Week5-IntroToOOP/3.Wednesday/Warmup/src/main/java/org/example/Warmup.java
+++ Week5-IntroToOOP/3.Wednesday/Warmup/src/main/java/org/example/Warmup.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import java.util.List;
+
 public class Warmup {
+    //Write a public method called findMax that takes an array of
+    // integers and returns the largest value as an int.
+
+    public int findMax(int[] arr){
+
+    }
+
+    private void printNames(List<String> names){
+
+    }
+
+    public double addNumbers(double a, double b){
+        return a + b;
+    }
+
+    private boolean isSystemActive(){
+
+    }
+
+    public List<Character> repeatCharacters(String name, int num){
+
+    }
+
+    private void showWelcomeMessage(){
+
+    }
+
+    public String joinStrings(String[] stringArray){
+
+    }
+
+    private void filterBelowThreshold(List<Integer> integers, int int2){
+        
+    }
 }
---
+++ Week5-IntroToOOP/4.Thursday/Warmup/src/main/java/org/example/BankAccount.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+    private static final double OVERDRAFT_LIMIT = -200;  // Can’t go below -200
+    private static final double OVERDRAFT_FEE = 20;
+
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        double potentialBalance = balance - amount;
+
+        if (potentialBalance >= 0) {
+            balance = potentialBalance;
+            return true;
+        } else if (potentialBalance >= OVERDRAFT_LIMIT) {
+            applyOverdraftFee(potentialBalance);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void applyOverdraftFee(double potentialBalance) {
+        balance = potentialBalance - OVERDRAFT_FEE;
+    }
+}
