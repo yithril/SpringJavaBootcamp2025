def7476c870c9357753d8e3d268f30e4c968d00e
yithril <yithril@gmail.com>
2025-05-08 00:06:49 -0400

    change

--- Week5-IntroToOOP/4.Thursday/Warmup/src/main/java/org/example/BankAccount.java
+++ Week5-IntroToOOP/4.Thursday/Warmup/src/main/java/org/example/BankAccount.java
@@ -20,6 +20,9 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
         balance += amount;
     }
 
