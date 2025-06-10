fff7e41669be6f61ca2dc7cbe47fa79279b34fce
yithril <yithril@gmail.com>
2025-06-10 15:12:33 -0400

    example

--- Instructor-Workshop/Workshop5-VehicleDealership-InstructorVersion/src/main/java/org/example/Main.java
+++ Instructor-Workshop/Workshop5-VehicleDealership-InstructorVersion/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
