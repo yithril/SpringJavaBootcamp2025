8952457062ad60b575f8128e07e1c581614d9194
yithril <yithril@gmail.com>
2025-06-17 11:39:35 -0400

    example

--- API-StarterKit/demo/src/main/java/com/startkit/demo/controllers/ProductController.java
+++ API-StarterKit/demo/src/main/java/com/startkit/demo/controllers/ProductController.java
@@ -5,9 +5,7 @@ import com.startkit.demo.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,38 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAll(){
         var products = productDao.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    //endpoint would be /products/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable int id){
+        Product product = productDao.getProductById(id);
+
+        if(product == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+    }
+
+    //delete products
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        Product product = productDao.getProductById(id);
+
+        if(product == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        boolean successfullyDeleted = productDao.deleteProduct(id);
+
+        if(successfullyDeleted){
+            //204 NO CONTENT
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
