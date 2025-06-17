411fb33269a1a7c04467485fa9b484f5e57ae060
yithril <yithril@gmail.com>
2025-06-17 08:22:35 -0400

    DI

---
+++ Week11-API/2.Tuesday/DependencyInjection/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Your mission, should you choose to accept it
+        //Is to instantiate two cars. One with an eletric engine
+        //And one with a Gasoline Engine. Good luck.
+    }
+}
